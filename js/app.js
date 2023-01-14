82262f3660be6ec1ac76a31fa0fc1fc5de0b4dbb
bator <batr.fly@mail.ru>
2023-01-14 14:47:27 +0400

    add webpack

--- js/app.js
+++
@@ -1,7 +0,0 @@
-import { addProfileCards } from './profileCards.js'
-import { initOpenPopups } from './popup.js'
-import { initHandleFormSubmit } from './form.js'
-
-addProfileCards()
-initOpenPopups()
-initHandleFormSubmit()
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import { addProfileCards } from "./profileCards.js";
+import { initOpenPopups } from "./popup.js";
+import { initHandleFormSubmit } from "./form.js";
+import "../pages/index.css";
+
+addProfileCards();
+initOpenPopups();
+initHandleFormSubmit();
